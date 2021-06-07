e60c60789d8ba28d224ebeae1ef768a31204dbcf
gayatri0728 <85476704+gayatri0728@users.noreply.github.com>
2021-06-07 11:57:58 +0530

    Add files via upload

---
+++ N2.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function N2() {
+  return (
+    <div className="note">
+      <h1>Web-Developement Using HTML,CSS</h1>
+      <p>
+        This is a amazing Bootcamp on Web Developement. I learned many things regarding web developement.
+        Technology Used: HTML,CSS,Bootstrap
+      </p>
+    </div>
+  );
+}
+
+export default N2;
---
+++ N3.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function N3() {
+  return (
+    <div className="note">
+      <h1>web-Developement using react</h1>
+      <p>
+      This is a amazing Bootcamp on Web Developement. I learned many things regarding web developement and the amazing framework called react.
+        Technology Used: React,Javascript </p>
+    </div>
+  );
+}
+
+export default N3;
